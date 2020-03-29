3793890fd3ced635ba04b2ddee2922e2662a7148
Dave Nolan <dpn.nolan@gmail.com>
2020-03-29 13:56:06 -0400

    first commit, seed colors

---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Palette</h1>
+            </div>
+        )
+    }
+}
