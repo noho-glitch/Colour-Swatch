c987c00b38e7e3800c59badd106462f0a3830977
Dave Nolan <dpn.nolan@gmail.com>
2020-04-08 13:07:50 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./components/Palette/Palette";
 import seedColors from "./utils/seedColors";
 import { generatePalette } from "./utils/colorHelpers";
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[1])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>INDIVIDUAL PALETTE</h1>}
+      />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[1])} />
+    // </div>
   );
 }
 
