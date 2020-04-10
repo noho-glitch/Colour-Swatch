ada85666af57b30382694edd6864bdb8de688e64
Dave Nolan <dpn.nolan@gmail.com>
2020-04-10 12:41:30 -0400

    created separate styles files

--- src/components/PaletteList/PaletteList.js
+++ src/components/PaletteList/PaletteList.js
@@ -1,37 +1,9 @@
 import React, { Component } from "react";
 import MiniPalette from "../MiniPalette/MiniPalette";
-import { Link } from "react-router-dom";
+import styles from "./PaletteListStyles"
 import { withStyles } from "@material-ui/styles";
 
-const styles = {
-    root: {
-      backgroundColor: "blue",
-      height: "100vh",
-      display: "flex",
-      alignItems: "flex-start",
-      justifyContent: "center"
-    },
-    container: {
-      width: "50%",
-      display: "flex",
-      alignItems: "flex-start",
-      flexDirection: "column",
-      flexWrap: "wrap"
-    },
-    nav: {
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between"
-    },
-    palettes: {
-      boxSizing: "border-box",
-      width: "100%",
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 30%)",
-      gridGap: "5%"
-    }
-  };
-  
+
   class PaletteList extends Component {
     goToPalette(id) {
       this.props.history.push(`/palette/${id}`);
--- src/components/SingleColorPalette/SingleColorPalette.js
+++ src/components/SingleColorPalette/SingleColorPalette.js
@@ -1,51 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styles from "./SingleColorPaletteStyles"
 import { withStyles } from "@material-ui/styles";
 import Navbar from "../Navbar/Navbar"
 import ColorBox from "../ColorBox/ColorBox";
 import PaletteFooter from "../PaletteFooter/PaletteFooter"
-
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column"
-  },
-  colors: {
-    height: "90%"
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    opacity: 1,
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      border: "none",
-      textDecoration: "none"
-    }
-  }
-};
 
 class SingleColorPalette extends Component {
   constructor(props) {
