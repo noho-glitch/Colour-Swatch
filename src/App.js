78c60e020b96b11ceba5899f927f6dde082e00e0
Dave Nolan <dpn.nolan@gmail.com>
2020-04-17 12:26:23 -0400

    added save new palette button

--- src/App.js
+++ src/App.js
@@ -8,18 +8,33 @@ import seedColors from "./utils/seedColors";
 import { generatePalette } from "./utils/colorHelpers";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { palettes: seedColors };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
   findPalette(id) {
-    return seedColors.find(function(palette) {
+    return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
   }
   render() {
     return (
       <Switch>
-        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
-          path="/palette/:paletteId/:colorId"
+          path='/palette/new'
+          render={routeProps => (
+            <NewPaletteForm savePalette={this.savePalette} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path='/palette/:paletteId/:colorId'
           render={routeProps => (
             <SingleColorPalette
               colorId={routeProps.match.params.colorId}
@@ -31,14 +46,14 @@ class App extends Component {
         />
         <Route
           exact
-          path="/"
+          path='/'
           render={routeProps => (
-            <PaletteList palettes={seedColors} {...routeProps} />
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
           )}
         />
         <Route
           exact
-          path="/palette/:id"
+          path='/palette/:id'
           render={routeProps => (
             <Palette
               palette={generatePalette(
