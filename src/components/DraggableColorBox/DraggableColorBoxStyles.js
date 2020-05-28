d8ee73bacd092609bafd75d7f4ad6af76c45e010
Dave Nolan <dpn.nolan@gmail.com>
2020-05-28 17:12:44 -0400

    made form navbar responsive

--- src/components/DraggableColorBox/DraggableColorBoxStyles.js
+++ src/components/DraggableColorBox/DraggableColorBoxStyles.js
@@ -1,34 +1,47 @@
+import sizes from "../../utils/Sizes";
 
 const styles = {
-    root: {
-      width: "20%",
-      height: "25%",
-      margin: "0 auto",
-      display: "inline-block",
-      position: "relative",
-      cursor: "pointer",
-      marginBottom: "-3.5px",
-      "&:hover svg": {
-        color: "white",
-        transform: "scale(1.5)"
-      }
+  root: {
+    width: "20%",
+    height: "25%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-3.5px",
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.5)"
     },
-    boxContent: {
-      position: "absolute",
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "20%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "10%"
+    },
+    [sizes.down("sm")]: {
       width: "100%",
-      left: "0px",
-      bottom: "0px",
-      padding: "10px",
-      color: "rgba(0, 0, 0, 0.5)",
-      letterSpacing: "1px",
-      textTransform: "uppercase",
-      fontSize: "12px",
-      display: "flex",
-      justifyContent: "space-between"
-    },
-    deleteIcon: {
-      transition: "all 0.3s ease-in-out"
+      height: "5%"
     }
-  };
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out"
+  }
+};
 
-  export default styles;
+export default styles;
