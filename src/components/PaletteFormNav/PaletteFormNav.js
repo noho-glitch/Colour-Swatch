3af2f624a642200cbc40371906b8eab3cf1fdafe
Dave Nolan <dpn.nolan@gmail.com>
2020-06-09 15:08:44 -0400

    added classNames to colorBox

--- src/components/PaletteFormNav/PaletteFormNav.js
+++ src/components/PaletteFormNav/PaletteFormNav.js
@@ -9,8 +9,6 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Button from "@material-ui/core/Button";
 import styles from "./PaletteFormNavStyles";
 
